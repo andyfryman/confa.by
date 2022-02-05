7f62786af2ef415ac59e972edb79d21b574332ee
Oleg Safonov <osafonov@ibagroup.eu>
2022-02-05 15:34:42 +0300

    feat: add index, event and terms pages

---
+++ src/js/main.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.addEventListener('scroll', function(e) {
+        const element = document.querySelector("header.header");
+        const button = document.querySelector(".program_button_wrapper");
+    
+        if (window.scrollY > 0) {
+            element.classList.add("scrolled");
+        } else {
+            element.classList.remove("scrolled");
+        }
+    
+        if (window.scrollY > 100) {
+            button.classList.add("visible");
+        } else {
+            button.classList.remove("visible");
+        }
+    });
+    
+    document.addEventListener('click', function(e) {
+        e = e || window.event;
+        const target = e.target;
+        const elem = document.querySelector(".navigation_mobile_button");
+    
+        if (target !== elem) {
+            const element = document.querySelector("div.navigation_tabs");
+            element.classList.remove("visible");
+        }
+    
+    }, false);
+    
+    function navigationButtonClick() {
+        const element = document.querySelector("div.navigation_tabs");
+        if (element.classList.contains("visible")) {
+            element.classList.remove("visible");
+        } else {
+            element.classList.add("visible");
+        }
+    }
+    
+    const menuButton = document.querySelector("div.navigation_mobile_button")
+    menuButton.addEventListener("click", () => navigationButtonClick());
+});
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+    output: {
+        filename: '[name]',
+        path: path.resolve(__dirname, 'docs'),
+    },
+    entry: {
+        "bundle.js": [path.resolve(__dirname, "src", "js", "main.js")],
+        "bundle.css": [path.resolve(__dirname, "src", "css", "style.css")]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "bundle.min.css"
+        }),
+        new CopyPlugin({
+            patterns: [
+              { from: "src/assets/*", to: "assets/[name][ext]" },
+              { from: "src/*.html", to: "[name][ext]" },
+            ],
+        }),
+    ],
+    optimization: {
+        minimizer: [
+          new CssMinimizerPlugin({
+            minify: CssMinimizerPlugin.cleanCssMinify,
+          }),
+        ],
+        minimize: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
+};
